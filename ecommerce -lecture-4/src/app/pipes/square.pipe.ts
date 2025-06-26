6646e9b21db853f887173ac286b9af5c82af5702
FAWRY\Mohamed.Amgad <Mohamed.Amgad@fawry.com>
2025-06-26 18:49:05 +0300

    fix: add actual lecture 4 Angular project files

---
+++ ecommerce -lecture-4/src/app/pipes/square.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'square',
+  standalone: true,
+})
+export class SquarePipe implements PipeTransform {
+  transform(value: number, power: number = 2): number {
+    return Math.pow(value, power);
+  }
+}
